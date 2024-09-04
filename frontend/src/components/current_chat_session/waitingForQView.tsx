d62d5cca5c4c9a4c20be361b1aaddc071326181d
Bobby Lawson <bobby.lawson17@gmail.com>
2024-09-03 19:01:26 -0500

    added a margin on the sides of the waiting screen area

--- frontend/src/components/current_chat_session/waitingForQView.tsx
+++ frontend/src/components/current_chat_session/waitingForQView.tsx
@@ -4,7 +4,7 @@ import GradientDotsAnimation from '../animation_views/gradientDotsAnimation';
 function WaitingView() {
     return (
         <>
-            <div className='d-flex flex-column'>
+            <div className='d-flex flex-column mx-5'>
                 <h3 className='chatbot-name d-flex justify-content-center'> Ask a question to start a chat ...</h3>
                 <GradientDotsAnimation/>
             </div>
