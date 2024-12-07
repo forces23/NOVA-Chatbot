ac353dedd42efd22b000e86e0c8da74e945d277e
Bobby Lawson <bobby.lawson17@gmail.com>
2024-12-07 17:26:36 -0600

    tyring to understand the issue with the mobile vs desktop view issue

---
+++ frontend/src/App copy 2.tsx
@@ -0,0 +1,32 @@
+import Chatbot from './components/chatbot';
+import TitleBar from './components/title_bar/titleBar';
+import SidePane from './components/side_pane/sidePane';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+// import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Game_A from './games/game-a';
+
+
+function HomePage() {
+  return (
+    <div className="">
+      hello world!!!!!
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        {/* Add additional routes here */}
+        <Route path='/have-fun-game-a' element={<Game_A />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
